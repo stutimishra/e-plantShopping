2acca50c9e5fd1f8af89ddaab6d5098258933d4b
stutimishra <stutimishra1999dec@gmail.com>
2025-04-20 13:18:29 -0400

    task 1

--- src/CartSlice.jsx
+++ src/CartSlice.jsx
@@ -1,4 +1,3 @@
-// CartSlice.jsx
 import { createSlice } from '@reduxjs/toolkit';
 
 export const CartSlice = createSlice({
@@ -8,21 +7,17 @@ export const CartSlice = createSlice({
   },
   reducers: {
     addItem: (state, action) => {
-      // Add the selected plant to the cart
-      const item = action.payload;
-      const exists = state.items.find(i => i.name === item.name);
-      if (!exists) {
-        state.items.push(item);
-      }
+    
     },
     removeItem: (state, action) => {
-      // to be implemented
     },
     updateQuantity: (state, action) => {
-      // to be implemented
+
+    
     },
   },
 });
 
 export const { addItem, removeItem, updateQuantity } = CartSlice.actions;
+
 export default CartSlice.reducer;
